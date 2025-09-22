b6dd6ac9f9115669cb2c56fb12a305efc864e93a
Ethan <bocoboethan@gmail.com>
2025-09-22 16:26:33 +0800

    Update toDoApp.py

--- toDoApp.py
+++ toDoApp.py
@@ -1,46 +1,55 @@
 # toDoApp.py
-# Applied Black formatter 
 
 tasks = []
 
-
-def addtask(task):
+def addTask(task):
+    '''
+    This Function appends the input/task to the stack
+    '''
     tasks.append(task)
-    print("task added!")
-
+    print("task added!\n")
 
 def showTasks():
+    '''
+    This Function shows current tasks.
+    '''
     if len(tasks) == 0:
-        print("no tasks yet")
+        print("\nNo tasks yet.")
     else:
+        print("\nCurrent Tasks: ")
         for i in range(len(tasks)):
             print(i + 1, ".", tasks[i])
+        print("\n")
 
-
-def removetask(tasknumber):
+def removeTask(tasknumber):
+    '''
+    This Function removes/pops a task depending on the user.
+    '''
     tasks.pop(tasknumber - 1)
-    print("task removed!!")
-
+    print("task removed!\n")
 
 def main():
+    '''
+    A function called main for readability and modularity
+    '''
     while True:
         print("1. Add Task")
         print("2. Show Tasks")
         print("3. Remove Task")
         print("4. Exit")
-        ch = input("enter choice : ")
+        ch = input("\nEnter choice: ")
         if ch == "1":
-            t = input("enter task : ")
-            addtask(t)
+            t = input("Enter task : ")
+            addTask(t)
         elif ch == "2":
             showTasks()
         elif ch == "3":
             showTasks()  
             if tasks:  
                 try:
-                    n = int(input("enter task no to remove: "))
+                    n = int(input("Enter task number to remove: "))
                     if 1 <= n <= len(tasks):
-                        removetask(n)
+                        removeTask(n)
                     else:
                         print("Invalid task number!")
                 except ValueError:
@@ -48,10 +57,11 @@ def main():
             else:
                 print("No tasks to remove!")
         elif ch == "4":
+            print("Bye!\n")
             break
         else:
-            print("wrong choice!!")
-        print() 
+            print("Wrong choice !")
+            print() 
 
-
-main()
+if __name__ == "__main__":
+    main()

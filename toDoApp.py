086adf42c1c853ef71f9ac4efbb34732b1db836c
Gabriel Mortel <xanderbatman22@gmail.com>
2025-09-22 16:01:09 +0800

    Fixing the remove task.

--- toDoApp.py
+++ toDoApp.py
@@ -1,5 +1,6 @@
 # toDoApp.py
 # Applied Black formatter 
+
 tasks = []
 
 
@@ -17,16 +18,16 @@ def showTasks():
 
 
 def removetask(tasknumber):
-    tasks.pop(tasknumber)
+    tasks.pop(tasknumber - 1)
     print("task removed!!")
 
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
         ch = input("enter choice : ")
         if ch == "1":
             t = input("enter task : ")
@@ -34,12 +35,23 @@ def main():
         elif ch == "2":
             showTasks()
         elif ch == "3":
-            n = int(input("enter task no to remove: "))
-            removetask(n)
+            showTasks()  
+            if tasks:  
+                try:
+                    n = int(input("enter task no to remove: "))
+                    if 1 <= n <= len(tasks):
+                        removetask(n)
+                    else:
+                        print("Invalid task number!")
+                except ValueError:
+                    print("Please enter a valid number!")
+            else:
+                print("No tasks to remove!")
         elif ch == "4":
             break
         else:
             print("wrong choice!!")
+        print() 
 
 
 main()
